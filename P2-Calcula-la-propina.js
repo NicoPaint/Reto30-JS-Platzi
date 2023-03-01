b76fda43c45faf4b6af9b3251f82ff223c763ab5
NicoPaint <90674075+NicoPaint@users.noreply.github.com>
2023-03-01 13:53:33 -0600

    Create P2-Calcula-la-propina.js
    Se realizo el segundo Playground. día 2, P2

---
+++ P2-Calcula-la-propina.js
@@ -0,0 +1,29 @@
+/*
+En este desafío tendrás que calcular la propina que deben dejar los clientes de un restaurante en función de su consumo.
+
+Recibirás 2 parámetros:
+
+billAmount: El costo total de lo que hayan consumido.
+tipPercentage: El porcentaje de propina que deban dejar.
+Ambos valores serán de tipo Number.
+Los valores serán siempre positivos incluyendo el 0.
+deberá devolver el valor de la propina como un número.
+Tendrás inputs y outputs como los siguientes 👇
+
+Ejemplo 1:
+
+Input: calculateTip(100, 10);
+Output: 10;
+
+Ejemplo 2:
+
+Input: calculateTip(1524.33, 25);
+Output: 381.0825;
+*/
+
+//Mi solucion.
+export function calculateTip(billAmount, tipPercentage) {
+  // Tu código aquí 👈
+  const tip = billAmount * tipPercentage / 100;
+  return tip;
+}
