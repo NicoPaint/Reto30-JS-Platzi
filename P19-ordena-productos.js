12253e93a83059ecdbb16fade0c10e25ef08f839
NicoPaint <felipepinve@gmail.com>
2023-03-18 14:32:22 -0500

    Se realizó el diecinueveavo playground del reto. Día 14, P19

---
+++ P19-ordena-productos.js
@@ -0,0 +1,51 @@
+/* 
+En este desafío, tendrás que ordenar una lista de productos.
+
+Tu tarea es implementar la lógica de la función sortByAvailabilityAndPrice. Esta función recibirá un array de objetos que representan productos, y devolverá una copia ordenada de dicho array.
+
+El ordenamiento se realizará siguiendo dos criterios:
+
+Primero, los productos disponibles en inventario serán colocados al principio de la lista.
+Luego, los productos serán ordenados por su precio, de manera ascendente.
+Es importante destacar que la lista original no sufrirá ninguna modificación, y que la función devolverá una nueva lista con los cambios mencionados.
+
+Ejemplo
+
+Input:
+
+const products = [
+  { name: "product1", price: 10, inStock: true },
+  { name: "product2", price: 20, inStock: false },
+  { name: "product3", price: 15, inStock: true },
+  { name: "product4", price: 5, inStock: false },
+]
+
+sortByAvailabilityAndPrice(products)
+
+Output:
+[
+  { name: "product1", price: 10, inStock: true },
+  { name: "product3", price: 15, inStock: true },
+  { name: "product4", price: 5, inStock: false },
+  { name: "product2", price: 20, inStock: false },
+]
+*/
+
+//Mi solución
+export function sortByAvailabilityAndPrice(products) {
+    // Tu código aquí 👈
+    const organizedProducts = [...products];
+
+    organizedProducts.sort((a, b) => a.price - b.price).sort((a, b) => b.inStock - a.inStock);
+
+    return organizedProducts;
+}
+
+const products = [
+    { name: "product1", price: 10, inStock: true },
+    { name: "product2", price: 20, inStock: false },
+    { name: "product3", price: 15, inStock: true },
+    { name: "product4", price: 5, inStock: false },
+]
+
+sortByAvailabilityAndPrice(products);
