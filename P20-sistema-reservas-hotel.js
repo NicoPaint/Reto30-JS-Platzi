e809ad7f5986117d0247711933148f00c448cf75
NicoPaint <felipepinve@gmail.com>
2023-04-08 10:40:55 -0500

    Se terminó el veinteavo playground del reto. Día 15, P20.

--- P20-sistema-reservas-hotel.js
+++ P20-sistema-reservas-hotel.js
@@ -164,6 +164,7 @@ function hotelSystem(rooms) {
             if(isOccupied) throw new Error("La habitación no está disponible");
 
             reservaciones.push(reservation);
+            return `La reservación de ${reservation.name} fue agendada exitosamente`;
         },
         removeReservation: id => {
           const removed = reservaciones.find(reserva => reserva.id === id);
@@ -177,7 +178,7 @@ function hotelSystem(rooms) {
         getReservations: () => reservaciones,
         getAvailableRooms: (checkIn, checkOut) => {
           const freeRooms = [];
-          
+
           const occupiedRooms = reservaciones.filter(reserva => comprobarFechas(reserva, checkIn, checkOut)).map(reserva => reserva.roomNumber).filter((roomNumber, index, array) => array.indexOf(roomNumber) === index);
           
           for(let i = 1; i <= rooms; i++){
@@ -217,4 +218,101 @@ myHotel.addReservation({
   roomNumber: 7,
 });
 myHotel.getAvailableRooms("01/09", "18/09");
+
+//La solución de Platzi
+
+export function hotelSystem(rooms) {
+  const reservations = [];
+
+  function searchReservation(id) {
+    const index = reservations.findIndex((room) => room.id === id);
+
+    if (index > -1) {
+      return reservations[index];
+    }
+
+    throw new Error("La reservación no existe");
+  }
+
+  function getSortReservations() {
+    const copy = [...reservations];
+
+    copy.sort((a, b) => {
+      const aDate = new Date(`${a.checkIn} ${new Date().getFullYear()}`);
+      const bDate = new Date(`${b.checkIn} ${new Date().getFullYear()}`);
+      return aDate - bDate;
+    });
+
+    return copy;
+  }
+
+  function addReservation(reservation) {
+    if (!isAvailable(reservation)) {
+      throw new Error("La habitación se encuentra ocupada");
+    }
+
+    reservations.push(reservation);
+    return `La reservación de ${reservation.name} fue agendada exitosamente`;
+  }
+
+  function removeReservation(id) {
+    const index = reservations.findIndex((room) => room.id === id);
+
+    if (index > -1) {
+      const removedReservation = reservations[index];
+      reservations.splice(index, 1);
+      return removedReservation;
+    }
+
+    throw new Error("La habitación que se busca remover no existe");
+  }
+
+  function getReservations() {
+    return reservations;
+  }
+
+  function isAvailable(reservation) {
+    const checkIn = reservation.checkIn;
+    const checkOut = reservation.checkOut;
+
+    for (const currentReservation of reservations) {
+      const currentCheckIn = currentReservation.checkIn;
+      const currentCheckOut = currentReservation.checkOut;
+
+      if (
+        (checkIn >= currentCheckIn && checkIn < currentCheckOut) ||
+        (checkOut > currentCheckIn && checkOut <= currentCheckOut) ||
+        (checkIn <= currentCheckIn && checkOut >= currentCheckOut)
+      ) {
+        if (currentReservation.roomNumber === reservation.roomNumber) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  function getAvailableRooms(checkIn, checkOut) {
+    const availableRooms = [];
+
+    for (let i = 1; i <= rooms; i++) {
+      const reservation = { checkIn, checkOut, roomNumber: i };
+
+      if (isAvailable(reservation)) {
+        availableRooms.push(i);
+      }
+    }
+    return availableRooms;
+  }
+
+  return {
+    searchReservation,
+    getSortReservations,
+    addReservation,
+    removeReservation,
+    getReservations,
+    getAvailableRooms,
+  };
+}
 
